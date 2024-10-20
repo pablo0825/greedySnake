b128c184344b6ebee9ce13415e7a2319f989b87c
pablo0825 <pablo0825@stust.edu.tw>
2024-10-20 21:55:10 +0800

    1020
    完成貪食蛇的專案，但有些程式碼的邏輯關聯沒有很清楚。
    要在之後整理專案的程式碼的時候，重新思考一次。

---
+++ app.js
@@ -0,0 +1,160 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const unit = 20;
+const row = canvas.height / unit;
+const column = canvas.width / unit;
+
+let snake = [];
+
+function createSnake() {
+  snake[0] = { x: 80, y: 0 };
+  snake[1] = { x: 60, y: 0 };
+  snake[2] = { x: 40, y: 0 };
+  snake[3] = { x: 20, y: 0 };
+}
+
+class Fruit {
+  constructor() {
+    this.x = Math.floor(Math.random() * column) * unit;
+    this.y = Math.floor(Math.random() * row) * unit;
+  }
+
+  drawFruit() {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(this.x, this.y, unit, unit);
+  }
+
+  pickALocation() {
+    let overlapping = false;
+    let new_x;
+    let new_y;
+
+    const checkOverlap = (new_x, new_y) => {
+      for (let i = 0; i < snake.length; i++) {
+        if (new_x == snake[i].x && new_y == snake[i].y) {
+          overlapping = true;
+          return;
+        }
+      }
+      overlapping = false;
+    };
+
+    do {
+      new_x = Math.floor(Math.random() * column) * unit;
+      new_y = Math.floor(Math.random() * row) * unit;
+      checkOverlap(new_x, new_y);
+    } while (overlapping);
+
+    this.x = new_x;
+    this.y = new_y;
+  }
+}
+
+createSnake();
+
+let myFruit = new Fruit();
+
+window.addEventListener("keydown", changeDirection);
+
+let d = "Right";
+
+function changeDirection(e) {
+  if (e.key == "ArrowRight" && d != "Left") {
+    d = "Right";
+  } else if (e.key == "ArrowDown" && d != "Up") {
+    d = "Down";
+  } else if (e.key == "ArrowLeft" && d != "Right") {
+    d = "Left";
+  } else if (e.key == "ArrowUp" && d != "Down") {
+    d = "Up";
+  }
+}
+
+let highestScore;
+loadHighestScore();
+
+let score = 0;
+document.getElementById("myScore").innerHTML = "遊戲分數：" + score;
+document.getElementById("myScore2").innerHTML = "最高分數：" + highestScore;
+
+function draw() {
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+      clearInterval(myGame);
+      alert("遊戲結束");
+      return;
+    }
+  }
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  myFruit.drawFruit();
+
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i == 0 ? "lightgreen" : "lightblue";
+    ctx.strokeStyle = "white";
+
+    if (snake[i].x >= canvas.width) {
+      snake[i].x = 0;
+    }
+
+    if (snake[i].x < 0) {
+      snake[i].x = canvas.width - unit;
+    }
+
+    if (snake[i].y >= canvas.height) {
+      snake[i].y = 0;
+    }
+
+    if (snake[i].y < 0) {
+      snake[i].y = canvas.height - unit;
+    }
+
+    ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
+    ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
+  }
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (d == "Left") {
+    snakeX -= unit;
+  } else if (d == "Up") {
+    snakeY -= unit;
+  } else if (d == "Right") {
+    snakeX += unit;
+  } else if (d == "Down") {
+    snakeY += unit;
+  }
+
+  let newHead = { x: snakeX, y: snakeY };
+
+  if (snake[0].x == myFruit.x && snake[0].y == myFruit.y) {
+    myFruit.pickALocation();
+    score++;
+    setHighestScore(score);
+    document.getElementById("myScore").innerHTML = "遊戲分數：" + score;
+    document.getElementById("myScore2").innerHTML = "最高分數：" + highestScore;
+  } else {
+    snake.pop();
+  }
+  snake.unshift(newHead);
+}
+
+let myGame = setInterval(draw, 100);
+
+function loadHighestScore() {
+  if (localStorage.getItem("highestScore") == null) {
+    highestScore = 0;
+  } else {
+    highestScore = Number(localStorage.getItem("highestScore"));
+  }
+}
+
+function setHighestScore(score) {
+  if (score > highestScore) {
+    localStorage.setItem("highestScore", score);
+    highestScore = score;
+  }
+}
